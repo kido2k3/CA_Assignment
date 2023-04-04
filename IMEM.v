c0c26186369800d61391a27d85a6f8c83ae58069
tuankiet <tuankiet0303@gmail.com>
2023-04-04 20:31:31 +0700

    cần xử lý lại module instruction memory

--- IMEM.v
+++ IMEM.v
@@ -20,21 +20,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module IMEM(
-input [7:0] IMEM_PC,
-//input clk,
-output [31:0] IMEM_instruction
-    );
-    reg [31:0] ins [0:63];
-    integer i;
-    initial begin
-        //$readmemb("input.mem", ins);
-        for(i=0; i<64 ;i=i+1)
-        begin
-            ins[i] = 0;
-        end
-        $readmemh("./input_text.txt", ins);
+// module IMEM(
+// input [7:0] IMEM_PC,
+// //input clk,
+// output [31:0] IMEM_instruction
+//     );
+//     reg [31:0] ins [0:63];
+//     integer i;
+//     initial begin
+//         //$readmemb("input.mem", ins);
+//         for(i=0; i<64 ;i=i+1)
+//         begin
+//             ins[i] = 0;
+//         end
+//         $readmemh("./input_text.txt", ins);
         
-    end
-    assign IMEM_instruction = ins[IMEM_PC>>2];
-endmodule
+//     end
+//     assign IMEM_instruction = ins[IMEM_PC>>2];
+// endmodule
