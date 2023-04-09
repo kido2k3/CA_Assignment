6b1fc3f9c0db7a0d8a6ea3482cbc955ff262f2e5
tuankiet <tuankiet0303@gmail.com>
2023-04-09 16:40:05 +0700

    sửa input nhầm file

--- IMEM.v
+++ IMEM.v
@@ -52,7 +52,7 @@ module IMEM(
         begin
             ins[i] = 0;
         end
-        $readmemh("C:/Users/tuankiet/Desktop/MIPS CPU/input_data.txt", ins);
+        $readmemh("C:/Users/tuankiet/Desktop/MIPS CPU/input_text.txt", ins);
         
     end
 endmodule
