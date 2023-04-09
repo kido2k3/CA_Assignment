1bb17b60a97496075dc09fb8adafa1ccfd8364dd
tuankiet <tuankiet0303@gmail.com>
2023-04-09 14:18:26 +0700

    thêm để test

--- IMEM.v
+++ IMEM.v
@@ -44,4 +44,15 @@ module IMEM(
 );
     reg [31:0] ins [0:63]; //có 64 câu lệnh, mỗi lệnh có 32 bit chứa trong 32 register
     assign IMEM_instruction = ins[IMEM_PC>>2];
+
+    integer i;
+    initial begin
+        //$readmemb("input.mem", ins);
+        for(i=0; i<64 ;i=i+1)
+        begin
+            ins[i] = 0;
+        end
+        $readmemh("./input_text.txt", ins);
+        
+    end
 endmodule
