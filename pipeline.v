732673abdb22fc3905dfed4ae6efab6ff05d46d0
tuankiet <tuankiet0303@gmail.com>
2023-04-11 00:06:57 +0700

    stall 2  khi trước là load

--- pipeline.v
+++ pipeline.v
@@ -127,7 +127,7 @@ module system(
             if      (!D_instruction[31:28]) //R
             begin
                 if (EX_instruction[20:16] == D_instruction[25:21] || EX_instruction[20:16] == D_instruction[20:16]) //rt == rs rt == rt
-                    D_stall_counter <= 2;
+                    D_stall_counter <= 2'b2;
                 else
                     D_stall_counter <= D_stall_counter;
 
@@ -137,7 +137,7 @@ module system(
             else if (D_instruction[31:28] == 4'b1000 || D_instruction[31:26] == 6'b001000 || D_instruction[31:28]==4'b1010) //load and addi and store
             begin
                 if (EX_instruction[20:16] == D_instruction[25:21])   //rt == rs
-                    D_stall_counter <= 2;
+                    D_stall_counter <= 2'b2;
                 else
                     D_stall_counter <= D_stall_counter;
             end
