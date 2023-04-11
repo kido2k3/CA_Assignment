ca772b46ced8b792bbf3756355930ed3ed33acbf
tuankiet <tuankiet0303@gmail.com>
2023-04-11 10:31:19 +0700

    fix IMEM

--- IMEM.v
+++ IMEM.v
@@ -43,15 +43,15 @@ module IMEM(
     output [31:0] IMEM_instruction //
 );
     parameter start_address = 32'h00400000 >> 2;
-    reg [31:0] ins [start_address : start_address + 63]; //có 64 câu lệnh, mỗi lệnh có 32 bit chứa trong 32 register
-    assign IMEM_instruction = ins[IMEM_PC >> 2];
+    reg [31:0] ins [0:63]; //có 64 câu lệnh, mỗi lệnh có 32 bit chứa trong 32 register
+    assign IMEM_instruction = ins[(IMEM_PC >> 2) - start_address];
 
     integer i;
     initial begin
         //$readmemb("input.mem", ins);
         for(i=0; i<64 ;i=i+1)
         begin
-            ins[ start_address + i] = 0;
+            ins[i] = 0;
         end
         $readmemh("C:/Users/tuankiet/Desktop/MIPS CPU/input_text.txt", ins);
         
