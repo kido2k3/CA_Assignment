d834fb40c503dc62f1a9e303ab0ea571ad55cfe1
tuankiet <tuankiet0303@gmail.com>
2023-04-16 09:57:47 +0700

    Sửa lại PC được truyền theo lệnh

--- pipeline.v
+++ pipeline.v
@@ -512,13 +512,13 @@ module decode_stage (
         else if (branch_taken || D_control_signal[10])  //lệnh jump và branch, giết câu lệnh tiếp theo
         begin
             D_instruction <= 0;
-            D_PC          <= F_PC - 4;
+            D_PC          <= F_PC;
         end
 
         else
         begin
             D_instruction <= F_instruction;
-            D_PC          <= F_PC - 4;
+            D_PC          <= F_PC;
         end
     end
     
