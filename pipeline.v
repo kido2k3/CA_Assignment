ba9b72db2a83399010f81bb12a730d9fb1cb02ab
tuankiet <tuankiet0303@gmail.com>
2023-04-16 17:53:07 +0700

    thêm forward cho store ở decode

--- pipeline.v
+++ pipeline.v
@@ -294,13 +294,26 @@ module system(
                     D_to_MEM_forwardSignal[0] <= 1'b0;                   //khong forward
             end
             
-            else if (D_instruction[31:28] == 4'b1000 || D_instruction[31:26] == 6'b001000 || D_instruction[31:28]==4'b1010) //load and addi and store
+            else if (D_instruction[31:28] == 4'b1000 || D_instruction[31:26] == 6'b001000) //load and addi
             begin
                 D_to_MEM_forwardSignal[0] <= 0;
                 if (MEM_instruction[15:11] == D_instruction[25:21])   //rd == rs
                     D_to_MEM_forwardSignal[1] <= 1'b1;                      
                 else
                     D_to_MEM_forwardSignal[1] <= 1'b0;
+            end
+
+            else if (D_instruction[31:28]==4'b1010) //store in Decode stage
+            begin //sw rt -> offset(rs)
+                if (MEM_instruction[15:11] == D_instruction[25:21])   //rd == rs
+                    D_to_MEM_forwardSignal[1] <= 1'b1;                      
+                else
+                    D_to_MEM_forwardSignal[1] <= 1'b0;
+                
+                if (MEM_instruction[15:11] == D_instruction[20:16])   //rd == rt
+                    D_to_MEM_forwardSignal[0] <= 1'b1;                      
+                else
+                    D_to_MEM_forwardSignal[0] <= 1'b0;
             end
 
             else
@@ -322,8 +335,20 @@ module system(
                     D_to_MEM_forwardSignal[0] <= 1'b0;
             end
             
+            else if (D_instruction[31:28]==4'b1010) //store in Decode stage
+            begin //sw rt -> offset(rs)
+                if (MEM_instruction[20:16] == D_instruction[25:21])   //rt == rs
+                    D_to_MEM_forwardSignal[1] <= 1'b1;                      
+                else
+                    D_to_MEM_forwardSignal[1] <= 1'b0;
+                
+                if (MEM_instruction[20:16] == D_instruction[20:16])   //rt == rt
+                    D_to_MEM_forwardSignal[0] <= 1'b1;                      
+                else
+                    D_to_MEM_forwardSignal[0] <= 1'b0;
+            end
 
-            else if (D_instruction[31:28] == 4'b1000 || D_instruction[31:26] == 6'b001000 || D_instruction[31:28]==4'b1010) //load and addi and store
+            else if (D_instruction[31:28] == 4'b1000 || D_instruction[31:26] == 6'b001000 ) //load and addi
             begin
                 D_to_MEM_forwardSignal[0] <= 0;
                 if      (MEM_instruction[20:16] == D_instruction[25:21])   //rd == rs
