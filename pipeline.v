e88768d9a107e48f9f868c9cfd05e6251f2ad1ec
tuankiet <tuankiet0303@gmail.com>
2023-05-09 09:52:54 +0700

    lỗi ở wb write register sai

--- pipeline.v
+++ pipeline.v
@@ -42,7 +42,7 @@ module system();
     initial
     begin 
         test_address_register = 9;
-        $monitor("time = %d, F_ins = %h, D_ins = %h, EX_ins = %h, WB_ins = %h, D_stall = %b, test value = %h, WB_exception = %b", $time, F_instruction, D_instruction, EX_instruction, WB_instruction, D_stall, test_value_register, WB_exception_signal);
+        $monitor("time = %d, F_ins = %h, D_ins = %h, EX_ins = %h, WB_ins = %h, D_stall = %b, WB_write_register = %d, WB_exception = %b", $time, F_instruction, D_instruction, EX_instruction, WB_instruction, D_stall, WB_write_register, WB_exception_signal);
     end
 
     //FETCH stage OK
